7d1578bea774f91482acfa5662b80e16a91c642e
d25higgins <d25higgins@gmail.com>
2018-07-29 19:39:20 -0700

    Added files

--- code.r
+++ code.r
@@ -1,1 +1,6 @@
 library("tidyverse")
+
+url <- "https://raw.githubusercontent.com/rafalab/dslabs/master/inst/extdata/murders.csv"
+dest_file <- "data/murders.csv"
+download.file(url, destfile = dest_file)
+
